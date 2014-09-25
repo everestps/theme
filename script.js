ae23be06172c261edb5eb6b6c8125b53aa3350e0
Alex B <me@alexb.ninja>
2014-09-25 09:04:58 -0700

    added line

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ $(window).keybind('keydown', {
   'c': function(){window.location.href = 'https://app.mysummitps.org/my/college/step/0'},
   'r': function(){window.location.href = 'https://app.mysummitps.org/my/reflections'},
   'g': function(){window.location.href = 'https://app.mysummitps.org/my/goals'},
+  'l': function(){$('.the-line').toggle()},
   'S-g': function(){window.location.href = 'https://app.mysummitps.org/my/grades'}
 });
 $('.icon-holder').remove()
